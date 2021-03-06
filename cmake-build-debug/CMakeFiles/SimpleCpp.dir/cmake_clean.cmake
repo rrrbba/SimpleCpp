f1ca873b3c73bfec19e3e47a3a2f6369dbf5c262
Rayven <rayven.burns@gmail.com>
2021-03-06 02:22:11 -0500

    added favoirte number

---
+++ cmake-build-debug/CMakeFiles/SimpleCpp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SimpleCpp.dir/main.cpp.obj"
+  "SimpleCpp.exe"
+  "SimpleCpp.exe.manifest"
+  "SimpleCpp.pdb"
+  "libSimpleCpp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SimpleCpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

27515e34b92bbca02aeb9ce65ec058e3fd00f496
Rayven <rayven.burns@gmail.com>
2021-03-10 02:01:35 -0500

    challenge 7 done

--- sec7chall.cpp
+++ sec7chall.cpp
@@ -20,6 +20,23 @@ int main() {
     cout << vector2.at(1) << endl;
     cout << vector2.size() << endl;
 
+    vector <vector<int>> vector_2d;
+    vector_2d.push_back(vector1);
+    vector_2d.push_back(vector2);
+    cout << vector_2d.at(0).at(0) << endl;
+    cout << vector_2d.at(0).at(1) << endl;
+    cout << vector_2d.at(1).at(0) << endl;
+    cout << vector_2d.at(1).at(1) << endl;
+
+    vector1.at(0) = 1000;
+
+    cout << vector_2d.at(0).at(0) << " " << vector_2d.at(0).at(1)<< endl;
+    cout << vector_2d.at(1).at(0) << " " << vector_2d.at(1).at(1)<< endl;
+
+    cout << vector1.at(0) << endl;
+
+    //The data in the vector didn't change after changing the data in vector1 because the vector is still referencing the data in the old vector for vector 1 (the copy that was made)
+
     return 0;
 }
 

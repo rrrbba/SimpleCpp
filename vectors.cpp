14c8b612e8f306ea5e1871b9e726f08d1c7e9f85
Rayven <rayven.burns@gmail.com>
2021-03-10 00:43:33 -0500

    added .at method for inserting elements into vector

--- vectors.cpp
+++ vectors.cpp
@@ -18,5 +18,13 @@ int main() {
 
     vector <double> hi_temps (365, 80.0); //(initial size of vector, what all 365 doubles are initialized to)
 
+    cout << vowels2[0] << endl;
+    //Accessing vector elements using vector syntax
+    cout << vowels2.at(0) << endl;
+    //Changing elements
+    cin >> vowels2.at(9);
+
+    //When do vectors grow as needed? Using push_back where vector automatically allocate the required space
+
     return 0;
 }

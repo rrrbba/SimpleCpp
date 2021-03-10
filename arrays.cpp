28d97334df23a36ab87ff642c01ddd0a9545b2c3
Rayven <rayven.burns@gmail.com>
2021-03-09 21:37:51 -0500

    getting address of array

--- arrays.cpp
+++ arrays.cpp
@@ -12,8 +12,13 @@ int main() {
     double hi_temps [] {90.1, 89.8, 77.5, 81.6};
     cout << "This first high temp is: " << hi_temps[0] << endl;
     hi_temps[0] = 100.7;
-    cout << "This first high temp is: " << hi_temps[0] << endl;
+    cout << "This first high temp is now: " << hi_temps[0] << endl;
 
+    int test_scores[5] {};
+    cout << "Enter first test score: " << endl;
+    cin >> test_scores[0];
+    cout << "First score at index 0: " << test_scores[0] << endl;
 
+    cout << "Memory address of array: " << test_scores << endl;
     return 0;
 };

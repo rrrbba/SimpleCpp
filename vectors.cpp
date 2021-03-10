1dd92e0e3de13cf2076f975b82ab589769089f2d
Rayven <rayven.burns@gmail.com>
2021-03-10 01:23:38 -0500

    using push back to add inputs into vectors

--- vectors.cpp
+++ vectors.cpp
@@ -17,12 +17,13 @@ int main() {
     vector <int> test_scores2 (10); //Parenthesis is constructor initial. syntax
 
     vector <double> hi_temps (365, 80.0); //(initial size of vector, what all 365 doubles are initialized to)
+    vector <int> test (3, 100);
 
     cout << vowels2[0] << endl;
     //Accessing vector elements using vector syntax
     cout << vowels2.at(0) << endl;
     //Changing elements
-    cin >> vowels2.at(4);
+//    cin >> vowels2.at(4);
 
     //When do vectors grow as needed? Using push_back where vector automatically allocate the required space
     vector <int> test_scores3 {100, 95, 99};
@@ -30,6 +31,20 @@ int main() {
     test_scores3.push_back(80); // 100, 95, 99, 80
     test_scores3.push_back(89); // 100, 95, 99, 80, 89
     cout << "Test_scores3: " << test_scores3[3] << endl;
+
+    int score_to_add {0};
+
+    cin >> score_to_add;
+    test_scores3.push_back(score_to_add);
+
+
+    cout << test_scores3.at(0) << endl;
+    cout << test_scores3.at(1) << endl;
+    cout << test_scores3.at(2) << endl;
+    cout << test_scores3.at(3) << endl;
+    cout << test_scores3.at(4) << endl;
+    cout << test_scores3.at(5) << endl;
+
 
     return 0;
 }

323a827dc530e37d956b2947ef94f28579f1c7d8
Rayven <rayven.burns@gmail.com>
2021-03-09 21:24:52 -0500

    added more array code

--- arrays.cpp
+++ arrays.cpp
@@ -5,6 +5,14 @@ using namespace std;
 
 int main() {
 
+    char vowels[] {'a','e','i','o','u'};
+    cout << "The first vowel is: " << vowels[0] << endl;
+    cout << "The last vowel is: " << vowels[4] << endl;
+
+    double hi_temps [] {90.1, 89.8, 77.5, 81.6};
+    cout << "This first high temp is: " << hi_temps[0] << endl;
+    hi_temps[0] = 100.7;
+    cout << "This first high temp is: " << hi_temps[0] << endl;
 
 
     return 0;
